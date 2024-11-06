dabd9d14cd01cbc964615f164f961034c0911253
David Cavalcanti <david_cavalcanti@outlook.com>
2024-11-06 17:45:43 -0300

    Exibir componente na página

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Meu app!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Banner />
+      <header className="App-header">        
       </header>
     </div>
   );
