0dca26416bc17570a3cb401a3e4c5963daee2fff
David Cavalcanti <david_cavalcanti@outlook.com>
2024-11-15 17:34:59 -0300

    Mapeando os colaboradores

--- organo/src/App.js
+++ organo/src/App.js
@@ -55,6 +55,7 @@ function App() {
         nome={time.nome}
         corPrimaria={time.corPrimaria}
         corSecundaria={time.corSecundaria}
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
       />)}
     </div>
   );
--- organo/src/components/Colaborador/index.js
+++ organo/src/components/Colaborador/index.js
@@ -1,14 +1,14 @@
 import './Colaborador.css'
 
-const Colaborador = () => {
+const Colaborador = ({ nome, imagem, cargo }) => {
     return (
         <div className='colaborador'>
             <div className='cabecalho'>
-                <img src='https://github.com/DavidCavalcanti.png' alt='' />
+                <img src={imagem} alt={nome} />
             </div>
             <div className='rodape'>
-                <h4>David Cavalcanti</h4>
-                <h5>Instrutor</h5>
+                <h4>{nome}</h4>
+                <h5>{cargo}</h5>
             </div>
         </div>
     )
