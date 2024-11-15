7f8573bf2963dfd9c7ae3710dd2eb841157de558
David Cavalcanti <david_cavalcanti@outlook.com>
2024-11-15 17:38:08 -0300

    Adicionar opção de time de programação

--- organo/src/App.js
+++ organo/src/App.js
@@ -7,6 +7,11 @@ function App() {
 
   const times = [
     {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9',
+    },
+    {
       nome: 'Front-End',
       corPrimaria: '#82CFFA',
       corSecundaria: '#E8F8FF',
